334f59703880eccae701ad8e6c013543848ed907
Mohamed Abolfadl <mabolfadl@gmail.com>
2022-03-13 14:54:10 +0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Default project setup based on cookiecutter',
+    author='MI',
+    license='MIT',
+)
